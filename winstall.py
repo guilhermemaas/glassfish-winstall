ee8f6382417d0f3ce00bde08bc94bc60ebbed255
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2019-06-10 13:24:21 -0300

    20190610 1322 pm
    20190610 1322 pm

--- winstall.py
+++ winstall.py
@@ -1,6 +1,7 @@
 import os
 import urllib.request
 import zipfile
+import subprocess
 
 def directory_verify(directory):
     if os.path.isdir(directory):
@@ -20,23 +21,38 @@ def descompact_zip(filepath, directory):
     zip_file = zipfile.ZipFile(f'{filepath}')
     zip_file.extractall(directory)
 
+def glassfish_create_service(bin_dir):
+    subprocess.run([bin_dir, 'create-service --name domain1'], capture_output=True)
+
 directory = r'C:\\glassfish_teste'
 url = 'http://download.oracle.com/glassfish/4.0/release/glassfish-4.0.zip'
 download_dir = f'{directory}\\download'
 descompact_file = f'{download_dir}\\glassfish-4.0.zip'
+bin_dir = f'{directory}\\glassfish4\\bin\\asadmin.bat'
 
 print('---')
 print(f'Diretório de instalação: {directory}')
 print(f'URL de download: {url}')
 print(f'Diretório de download: {download_dir}')
 print(f'Caminho do arquivo a ser descompactado: {descompact_file}')
-print('---')
+print(f'Caminho do diretório bin: {bin_dir}')
+print('---' * 10)
 
+print('Verificando se diretório existe.' + '\n' + '---' * 10)
 if directory_verify(directory) == False:
+    print('Diretório não existe, prosseguindo.' + '\n' + '---' * 10)
     create_dir(directory)
+    print(f'Diretório criado: {directory}, prosseguindo.' + '\n' + '---' * 10)
     create_dir(download_dir)
+    print(f'Diretório para download criado: {download_dir}, prosseguindo.' + '\n' + '---' * 10)
     download_glassfish4(download_dir, url)
+    print(f'Download realizado: {download_dir}, prosseguindo.' + '\n' + '---' * 10)
     descompact_zip(descompact_file, directory)
+    print(f'Zip descompactado: {descompact_file}, prosseguindo.' + '\n' + '---' * 10)
+    print(f'Criando serviço do Windows: ')
+    glassfish_create_service(bin_dir)
+    print('---' * 10)
 else:
     print('Diretório já existe ou já possui dados.')
 
+print('Finalizado.')
